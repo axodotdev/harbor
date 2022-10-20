8da57f8409e93de63502b62a7974357b7363e6ab
Sara Vieira <hey@iamsaravieira.com>
2022-10-20 18:38:06 +0200

    rename env variable

--- composables/useRedis.js
+++ composables/useRedis.js
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 
 export const useRedis = async () => {
   const client = createClient({
-    url: process.env.REDIS_URL,
+    url: process.env.REDIS_LINK,
     password: process.env.REDIS_PASSWORD,
   });
   client.on("error", (err) => console.log("Redis Client Error", err));
