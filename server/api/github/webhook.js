18a0b95d8f8298eeb07a0a1ffd07f4b215850109
Sara Vieira <hey@iamsaravieira.com>
2022-10-20 16:28:27 +0200

    simplify code

--- server/api/github/webhook.js
+++ server/api/github/webhook.js
@@ -1,31 +1,18 @@
 import { createNodeMiddleware, createProbot } from "probot";
 
-const app = async (app) => {
+function app(app) {
   app.on("issues.opened", async (context) => {
-    const response = await context.github.issues.listForRepo(
-      context.repo({
-        state: "all",
-        creator: context.payload.issue.user.login,
-      })
-    );
-
-    const countIssue = response.data.filter((data) => !data.pull_request);
-    if (countIssue.length === 1) {
-      try {
-        const config = await context.config("config.yml");
-        if (config && config.newIssueWelcomeComment) {
-          context.github.issues.createComment(
-            context.issue({ body: config.newIssueWelcomeComment })
-          );
-        }
-      } catch (err) {
-        if (err.code !== 404) {
-          throw err;
-        }
-      }
-    }
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    await context.octokit.issues.createComment(issueComment);
   });
-};
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
 
 export default createNodeMiddleware(app, {
   probot: createProbot(),
