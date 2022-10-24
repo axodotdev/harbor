4b751b1d0c3e83d4cf09ddfcb74017ab2d2cda1e
Sara Vieira <hey@iamsaravieira.com>
2022-10-24 14:36:12 +0200

    check what event is

--- server/api/github/webhook.js
+++ server/api/github/webhook.js
@@ -3,10 +3,11 @@
 import { createNodeMiddleware, createProbot } from "probot";
 
 function app(app) {
-  app.on(["pull_request.opened", "pull_request.edited"], async (context) => {
+  app.on(["check_run.completed"], async (context) => {
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
+    console.log(context);
     await context.octokit.issues.createComment(issueComment);
   });
   // For more information on building apps:
