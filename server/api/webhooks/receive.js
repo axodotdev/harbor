9b3ab9e24d167d6e3556d0f525997b87271f5817
Sara Vieira <hey@iamsaravieira.com>
2022-10-25 11:06:56 +0200

    add better error handling

--- server/api/webhooks/receive.js
+++ server/api/webhooks/receive.js
@@ -1,26 +1,35 @@
 import { v4 as uuidv4 } from "uuid";
 import { useRedis } from "../../../composables/useRedis";
-import { readBody, isMethod } from "h3";
+import { readBody, isMethod, sendError } from "h3";
 
 export default async (req) => {
   const body = await readBody(req);
   const isPost = isMethod(req, "POST");
 
-  // check where call comes from
+  try {
+    // check where call comes from
+    if (body && isPost) {
+      const client = await useRedis();
+      const id = uuidv4();
+      const parsedData = JSON.parse(Object.keys(body));
+      await client.set(id, JSON.stringify(parsedData));
+      const data = await client.get(id);
+      return {
+        id,
+        ...JSON.parse(data),
+      };
+    }
 
-  if (body && isPost) {
-    const client = await useRedis();
-    const id = uuidv4();
-    const parsedData = JSON.parse(Object.keys(body));
-    await client.set(id, JSON.stringify(parsedData));
-    const data = await client.get(id);
-    return {
-      id,
-      ...JSON.parse(data),
-    };
+    return sendError(req, {
+      statusCode: 500,
+      fatal: true,
+      statusMessage: "No data received",
+    });
+  } catch (e) {
+    return sendError(req, {
+      statusCode: 500,
+      fatal: true,
+      statusMessage: "There has been an issue reading your data",
+    });
   }
-
-  return {
-    error: "no json data received",
-  };
 };
