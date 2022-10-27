da8aa810a7673eec34903aa24e7ca29a7b43b74d
Sara Vieira <hey@iamsaravieira.com>
2022-10-27 23:37:48 +0200

    update json read

--- server/api/webhooks/receive.js
+++ server/api/webhooks/receive.js
@@ -1,18 +1,25 @@
 import { v4 as uuidv4 } from "uuid";
 import { useRedis } from "@/composables/useRedis";
-import { readBody, isMethod, sendError } from "h3";
+import { readBody, isMethod, sendError, getRequestHeaders } from "h3";
 
 export default async (req) => {
+  const headers = await getRequestHeaders(req);
   const body = await readBody(req);
   const isPost = isMethod(req, "POST");
 
   try {
+    if (!headers["user-agent"].includes("actions/github-script")) {
+      return sendError(req, {
+        statusCode: 401,
+        fatal: true,
+        statusMessage: "Unauthorized",
+      });
+    }
     // check where call comes from
     if (body && isPost) {
       const client = await useRedis();
       const id = uuidv4();
-      const parsedData = JSON.parse(Object.keys(body));
-      await client.set(id, JSON.stringify(parsedData));
+      await client.set(id, JSON.stringify(body));
       return {
         id,
       };
@@ -24,6 +31,7 @@ export default async (req) => {
       statusMessage: "No data received",
     });
   } catch (e) {
+    console.log(e);
     return sendError(req, {
       statusCode: 500,
       fatal: true,
