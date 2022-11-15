08d8bd9424755fce3523f36f50b8b56a6979b20c
Sarah GHP <sarah.groff.palermo@gmail.com>
2022-11-15 21:07:02 +0100

    Fix form persistence bug  using computed

--- components/AddANote.vue
+++ components/AddANote.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { computed, ref, watch } from "vue";
+import { computed, ref } from "vue";
 import { usePackageState } from "../composables";
 
 const props = defineProps({
   defaultNote: {
     type: String,
+    required: false,
     default: "",
   },
   name: {
@@ -13,12 +14,23 @@ const props = defineProps({
   },
 });
 
-const noteRef = ref(props.defaultNote);
+const tempFormValue = ref("");
 const { addNote } = usePackageState();
 
+const noteInnerText = computed(() => {
+  return tempFormValue.value || props.defaultNote;
+});
+
 const noteText = computed(() =>
-  noteRef.value ? "update note for this library" : "add a note for this library"
+  props.defaultNote
+    ? "update note for this library"
+    : "add a note for this library"
 );
+
+const updateFormText = (evt) => {
+  tempFormValue.value = evt.target.value;
+};
+
 const notePlaceholder = "add note";
 const buttonText = ref("+");
 
@@ -28,24 +40,10 @@ const buttonText = ref("+");
 */
 const saveNote = () => {
   buttonText.value = "...";
-  addNote({ pkg: props.name, note: noteRef.value });
+  addNote({ pkg: props.name, note: tempFormValue.value });
   setTimeout(() => (buttonText.value = "+"), 500);
+  tempFormValue.value = "";
 };
-
-/*
-  This makes it so that the noteRef resets when
-  the component is updated for a new criterion. I'm
-  not sure if there is a better way in the composition API
-  to deal with this, but given where we are now, I am
-  going to leave improving this as a TODO.
-*/
-watch(
-  () => props.name,
-  () => {
-    noteRef.value = props.defaultNote;
-    return;
-  }
-);
 </script>
 
 <template>
@@ -60,11 +58,12 @@ watch(
         <div class="flex gap-4 items-end">
           <textarea
             id="comment"
-            :value="noteRef"
+            :value="noteInnerText"
             :placeholder="notePlaceholder"
             rows="1"
             name="comment"
             class="bg-transparent outline-none border-0 w-full p-4 border-b-axo-orange border-b active:border-b-axo-pink focus:border-b-axo-pink focus:border-b-2 focus:outline-none focus:ring-0 placeholder:text-slate-400"
+            @input="updateFormText"
           />
           <button
             class="min-w-max bg-axo-orange text-xl hover:bg-axo-pink px-4 py-2 rounded-md"
