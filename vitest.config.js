26b5098700f7e93bde5cea33d003090c8653837d
Sara Vieira <hey@iamsaravieira.com>
2022-12-08 14:15:00 -0600

    more clean up

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,6 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   test: {
-    setupFiles: "./utils/test-setup.js",
     globals: true,
     environment: "happy-dom",
   },
