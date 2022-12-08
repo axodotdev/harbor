b074e4626d2f85ec62912ed2e832e08577f85a6c
Sara Vieira <hey@iamsaravieira.com>
2022-12-08 13:54:34 -0600

    use switch from fringe

--- components/ReportSwitch.vue
+++ components/ReportSwitch.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { computed, ref, watchEffect } from "vue";
-import AxoSwitch from "./Switch.vue";
+import { AxoSwitch } from "@axodotdev/fringe/lib";
 import { useSingleReport } from "../composables";
 import { useToggleApproval } from "../composables/useToggleApprovall";
 
